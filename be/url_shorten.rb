b589eb8fbb2981119d26dc7abf988f1de763f8a3
Devlon Duthie <duthied@gmail.com>
2017-08-24 13:58:30 -0400

    Adds a url shortening functionality to the back-end

---
+++ be/url_shorten.rb
@@ -0,0 +1,49 @@
+# url_shorten.rb
+# Encoding: utf-8
+require 'rubygems'  
+require 'bundler'
+require 'ostruct'
+
+Bundler.require
+
+require 'sinatra'
+require 'redis'
+require 'rack/conneg'
+
+require 'byebug'
+
+redis = Redis.new(host: ENV['REDIS_PORT_6379_TCP_ADDR'], port: ENV['REDIS_PORT_6379_TCP_PORT'])
+
+configure do  
+  set :server, :puma # default to puma for performance
+end  
+
+helpers do
+	include Rack::Utils
+	alias_method :h, :escape_html
+
+	def random_string(length)
+		rand(36**length).to_s(36)
+	end
+end
+
+get '/' do
+	@host_with_port = request.host_with_port
+	erb :index
+end
+
+post '/' do
+
+	@host_with_port = request.host_with_port
+	if params[:url] and not params[:url].empty?
+		@shortcode = random_string 5
+		redis.setnx "links:#{@shortcode}", params[:url]
+	end
+	erb :index
+end
+
+get '/:shortcode' do
+	@host_with_port = request.host_with_port
+	@url = redis.get "links:#{params[:shortcode]}"
+	redirect @url || '/'
+end
